62cd6d6de27a258ac35463ef407df7dee7da8197
lowzj <zj3142063@gmail.com>
2017-08-17 10:39:44 +0800

    update interleavingString.cpp

--- interleavingString.cpp
+++ interleavingString.cpp
@@ -6,40 +6,39 @@ class Solution {
         int len1;
         int len2;
         int len3;
-        int *m;
+        int **m;
     public:
-        int solve(int i, int j, string& s1, string& s2, string& s3) {
-            cout << i << " " << j << endl;
-            if (i+j == len3) return 1;
-            cout << i << " " << j << "a" << m[i*len1+j] << endl;
-            if (m[i*len1+j] != 0) return m[i*len1+j];
-            cout << i << " " << j << "b" << endl;
+        int solve(int r, int c, string& s1, string& s2, string& s3) {
+            if (r+c == len3) return 1;
+            if (m[r][c] != 0) return m[r][c];
 
             int tmp = -1;
-            if (i < len1 && s1[i] == s3[i+j]) {
-                tmp = solve(i+1, j, s1, s2, s3);
+            if (r < len1 && s1[r] == s3[r+c]) {
+                tmp = solve(r+1, c, s1, s2, s3);
             }
-            if (tmp != 1 && j < len2 && s2[j] == s3[i+j]) {
-                tmp = solve(i, j+1, s1, s3, s3);
+            if (tmp != 1 && c < len2 && s2[c] == s3[r+c]) {
+                tmp = solve(r, c+1, s1, s2, s3);
             }
-            m[i*len1+j] = tmp;
-            cout << i << " " << j << "c" << tmp << endl;
-            return m[i*len1+j];
+            m[r][c] = tmp;
+            return m[r][c];
         }
         bool isInterleave(string s1, string s2, string s3) {
             len1 = s1.size();
             len2 = s2.size();
             len3 = s3.size();
-            m = new int((len1+1)*(len2+1));
-            memset(m, 0, (len1+1)*(len2+1));
+            m = new int*[len1+1];
+            for (int i = 0; i <= len1; ++i) {
+                m[i] = new int[len2+1];
+                for (int j = 0; j <= len2; ++j) m[i][j] = 0;
+            }
 
             return (len1+len2 == len3) && solve(0, 0, s1, s2, s3) == 1;
         }
 };
 int main(void) {
-    string s1("aabd");
-    string s2("adbc");
-    string s3("aabdabdc");
+    string s1("aabcc");
+    string s2("dbbca");
+    string s3("aadbbcbcac");
     Solution s;
     cout << s.isInterleave(s1, s2, s3) << endl;
     return 0;
