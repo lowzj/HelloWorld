166333b6fcc1949130703a054e099e0714e5be55
lowzj <zj3142063@gmail.com>
2020-10-13 02:13:11 +0800

    nextPermutation: solution2
    Signed-off-by: lowzj <zj3142063@gmail.com>

--- go/nextPermutation.go
+++ go/nextPermutation.go
@@ -8,6 +8,7 @@ import (
 
 // https://leetcode-cn.com/problems/next-permutation/
 
+// solution 1
 // 思路:
 // 下面是演算了很久发现的规律, 下表是4个数字的全排列, 最右边是各排列中下标[0,1,2]对应的逆序对数.
 // idx   | 0 1 2 3 | 0 1 2
@@ -42,10 +43,10 @@ import (
 // 且 RO[i][k] 的进制为 n-k, 即当 RO[i][k] == n-k 时, 需向前一位(第k-1)进1.
 // 进位含义:
 //		在一个排列中从左至右的第 k 个数的最大逆序对数为 n-k-1, 即右侧数字的个数;
-//		当其逆序对数为 n-k 时, 需向前移动一位至 k-1.
+//		当其逆序对数为 n-k 时, 则进位至(k-1)继续计算 RO[i][k-1]+1;
 // RO[i][k]含义:
 //		除了逆序对数, 还可理解为排列 P[i][k] 在子数列 P[i][k,..,n-1] 升序排列后的下标.
-// 因为最后一位 P[i][n-1] 的逆序对数永远为0, 不用考虑.
+// 因为最后一位 P[i][n-1] 的逆序对数永远为0, 其进制为1, 这里省略.
 // 则排列 P[i] 的下一个排列 P[i+1] 的逆序对数为:
 // 	RO[i+1] = RO[i] + 1
 // 然后按如下步骤取数字形成排列 P[i+1]:
@@ -60,10 +61,6 @@ func nextPermutation(nums []int) {
 	order := make([]int, n)
 	copy(order, nums)
 	sort.Ints(order)
-	m := make(map[int]int)
-	for i := 0; i < n; i++ {
-		m[order[i]] = i
-	}
 
 	ro := make([]int, n)
 	for i := 0; i < n-1; i++ {
@@ -87,22 +84,75 @@ func nextPermutation(nums []int) {
 	}
 }
 
+// -----------------------------------------------------------------------------
+// solution 2
+// idx   | 0 1 2 3 4 5 6 7 | 0 1 2 3 4 5 6 7
+// ------+-----------------+----------------
+// P(10) | 1 2 3 4 6 8 5 7 | 0 0 0 0 1 2 0 0
+//                                        +1
+// P(11) | 1 2 3 4 6 8 7 5 | 0 0 0 0 1 2 1 0
+//                                        +1
+// P(12) | 1 2 3 4 7 5 6 8 | 0 0 0 0 2 0 0 0
+//                                        +1
+// P(13) | 1 2 3 4 7 5 8 6 | 0 0 0 0 2 0 1 0
+// 考察上面排列, 参考解法1中的逆序对数的进位含义, P[10]->P[11] 的转换实际上就是逆序对数增1:
+//   RO[10]+1 -> RO[11]
+// 而RO[11]相对于 RO[10] 变化的位为: RO[10][6,7], 就是 P[11] 相对于 P[10] 变化的位置.
+// P[11]->P[12]变化位置[4,5,6,7], 逆序对数同样变: RO[11][4,5,6,7]->R[12][4,5,6,7].
+// 增加最后移位逆序数方便理解, 0代表后续数组均为正序, 那么逆序对数增1可形象化理解为:
+//	* 从右至左, 找到第一个正序对P[i][j,j+1], 使得P[i][j]<P[i][j+1]
+//	* 在P[i][j+1,..,n-1]中找到最小的逆序对P[i][j,k], 其中j<k<=n-1, P[i][j]<P[i][k]
+//	* 交换j,k数据, 使得P[i][j]>P[i][k], 同时P[i][j+1,n-1]为全逆序对
+//	* 为了RO[i]只增1, 即进位效果, 将 P[i][j+1,n-1] 发转成为全正序对
+//	* 如上便得到下一个排列 P[i+1]
+
+func nextPermutation2(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for ; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
+			for j := n - 1; j > i; j-- {
+				if nums[i] < nums[j] {
+					nums[i], nums[j] = nums[j], nums[i]
+					break
+				}
+			}
+			break
+		}
+	}
+	for j, k := i+1, len(nums)-1; j < k; j, k = j+1, k-1 {
+		nums[j], nums[k] = nums[k], nums[j]
+	}
+}
+
 func main() {
 	cases := [][]int{
 		{1, 2, 3},
+		{1, 3, 2},
 		{3, 2, 1},
 		{1, 5, 1},
+		{1, 2, 3, 9, 4, 7, 10, 8, 5},
 	}
 	realCase := cases[0:]
 	for i, c := range realCase {
 		fmt.Println("## case", i)
 		fmt.Println("I:", c)
-		nextPermutation(c)
+		nextPermutation2(c)
 		fmt.Println("O:", c)
 	}
-	n := 4
+	reversedOrder()
+}
 
-	c := []int{4, 3, 2, 1}
+func reversedOrder() {
+	c := []int{8, 7, 6, 5, 4, 3, 2, 1}
+	n := len(c)
+	toStr := func(a []int) string {
+		s := ""
+		for i := 0; i < len(a); i++ {
+			s = s + " " + strconv.Itoa(a[i])
+		}
+		return s
+	}
 	f := func() string {
 		count := make([]int, len(c)-1)
 		for i := 0; i < n-1; i++ {
@@ -112,15 +162,11 @@ func main() {
 				}
 			}
 		}
-		s := ""
-		for i := 0; i < len(count); i++ {
-			s = s + " " + strconv.Itoa(count[i])
-		}
-		return s
+		return toStr(count)
 	}
-	for i := 0; i < 6*n; i++ {
-		nextPermutation(c)
-		fmt.Printf("// P(%2d)     %d %d %d %d |%s\n",
-			i, c[0], c[1], c[2], c[3], f())
+	for i := 0; i < n*n; i++ {
+		nextPermutation2(c)
+		fmt.Printf("// P(%2d) |%s |%s\n",
+			i, toStr(c), f())
 	}
 }
