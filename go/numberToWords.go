c34eb5ac8f9198ffb5f52adbfeaf3c158a4fb77f
lowzj <zj3142063@gmail.com>
2020-11-17 19:35:20 +0800

    add 数字的汉字读法
    Signed-off-by: lowzj <zj3142063@gmail.com>

--- go/numberToWords.go
+++ go/numberToWords.go
@@ -47,16 +47,66 @@ func numberToWords(num int) string {
 	return res
 }
 
-func main() {
-	cases := []int{
-		123,
-		1234567891,
+// 转成中文
+
+func read(n int64) string {
+	const zero = "零"
+	units := []string{"", "万", "亿", "兆"}
+	if n == 0 {
+		return zero
 	}
 
-	realCase := cases[0:]
+	result, useZero := "", false
+	for i, c := n, 0; i > 0; i /= 10000 {
+		tmp := readFour(int(i % 10000))
+		if tmp != zero {
+			if useZero {
+				result = zero + result
+			}
+			result = tmp + units[c] + result
+		}
+		useZero = i%10000/1000 <= 0 && len(result) > 0
+		c++
+	}
+	return result
+}
+
+func readFour(n int) string {
+	digits := []string{"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"}
+	units := []string{"", "十", "百", "千"}
+	if n == 0 {
+		return digits[0]
+	}
+	res, useZero := "", false
+	for i, c := n, 0; i > 0; i /= 10 {
+		idx := i % 10
+		if useZero && idx > 0 {
+			res = digits[0] + res
+		}
+		if idx > 0 {
+			res = digits[idx] + units[c] + res
+		}
+		useZero = !useZero && idx == 0 && len(res) > 0
+		c++
+	}
+	return res
+}
+
+func main() {
+	cases := []int64{
+		123,
+		1234567891,
+		1240567000,
+		1240560000,
+		1000000001,
+		1000000000000001,
+	}
+
+	realCase := cases[2:]
 	for i, c := range realCase {
 		fmt.Println("## case", i)
 		// solve
-		fmt.Println(numberToWords(c))
+		//fmt.Println(numberToWords(int(c)))
+		fmt.Println(read(c))
 	}
 }
