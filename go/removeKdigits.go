2047cd409c84578568e3f4b6fa73c116727b20b5
lowzj <zj3142063@gmail.com>
2020-11-25 19:10:02 +0800

    update 402. Remove K digits
    go/removeKdigits.go: add solution with increasing stack
    
    Signed-off-by: lowzj <zj3142063@gmail.com>

--- go/removeKdigits.go
+++ go/removeKdigits.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
 // https://leetcode-cn.com/problems/remove-k-digits/
-
+// 402. 移掉K位数字 | Remove K Digits
 //------------------------------------------------------------------------------
 // Solution 1
 // 正序遍历, 统计逆序对, 游标 i 处将 i 之前的所有大于 num[i] 的元素删除, 直至 k 次为止, 不够
@@ -88,6 +89,40 @@ func removeKdigits(num string, k int) string {
 	return string(res)
 }
 
+//------------------------------------------------------------------------------
+// Solution 0
+//
+// 要使删除 k 个数字后, 剩余数字最小, 那必须高位数字尽可能小.
+//
+// 递增栈: 递增存储待删除的数字.
+// 遍历 num, 当处理 num[i] 时, 循环比较栈顶与 num[i]:
+//   * 若 stack[top] > num[i], 说明高位数字比当前数大, 那么可以删掉栈顶, 留当前数字.
+//   * 若 stack[top] <= num[i], 停止循环, 将当前数字加入栈中, 待与数字比较, 以决定是否删除.
+//   * 因为指定删除 k 个数字, 所以若已经删了 k 个数字, 循环比较也要停止.
+// 遍历结束后, 若 k 大于 0(说明原数字很接近递增排列), 需要弹出 k 个数字. 最后留在栈中的数字就是
+// 结果. 注意, 要把前导零删掉, 如果只剩0, 不能全部删除, 要返回 "0".
+//
+// 复杂度分析:
+//   * 时间: O(N)
+//   * 空间: O(N)
+func removeKdigits0(num string, k int) string {
+	N := len(num)
+	st, p := make([]byte, N), 0
+
+	for i := 0; i <= N; i++ {
+		for ; p > 0 && k > 0 && (i == N || st[p-1] > num[i]); k-- {
+			p--
+		}
+		if i < N {
+			st[p], p = num[i], p+1
+		}
+	}
+	if res := string(bytes.TrimLeft(st[:p], "0")); res != "" {
+		return res
+	}
+	return "0"
+}
+
 func main() {
 	cases := []struct {
 		num string
@@ -106,5 +141,6 @@ func main() {
 		fmt.Println("## case", i)
 		// solve
 		fmt.Println(removeKdigits(c.num, c.k))
+		fmt.Println(removeKdigits0(c.num, c.k))
 	}
 }
