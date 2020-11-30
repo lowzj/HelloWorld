ddf3e4205799fdd8a5489c6829d1d708c3eec827
lowzj <zj3142063@gmail.com>
2020-11-30 21:17:24 +0800

    add 767. Reorganize String
    go/tree/reorganizeString.go
    
    Signed-off-by: lowzj <zj3142063@gmail.com>

--- go/reorganizeString.go
+++ go/reorganizeString.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // https://leetcode-cn.com/problems/reorganize-string/
@@ -12,43 +13,47 @@ import (
 // Solution
 //
 // 复杂度分析:
-//   * 时间:
-//   * 空间:
+//   * 时间: O(N+26lg26)
+//   * 空间: O(N)
 func reorganizeString(S string) string {
 	N := len(S)
-	f := [26]int{}
+	f := make([]int, 26)
 	for _, c := range S {
-		f[c-'a']++
+		f[c-'a'] = (f[c-'a']/100+1)*100 + int(c-'a')
 	}
-	res := ""
-	for k := 0; k < N; {
-		for i := 0; i < 26; i++ {
-			if f[i] <= 0 {
-				continue
+	sort.Ints(f)
+	res, k, start := make([]byte, N), 0, 0
+	for i := 25; i >= 0 && f[i] > 0; i-- {
+		v, b := f[i]/100, byte(f[i]%100+'a')
+		if N%2 == 0 && v > N/2 || N%2 == 1 && v > (N+1)/2 {
+			return ""
+		}
+		for j := v; j > 0; j-- {
+			res[k] = b
+			k += 2
+			if k >= N {
+				start++
+				k = start
 			}
-			if N%2 == 0 && f[i] > N/2 || N%2 == 1 && f[i] > (N+1)/2 {
-				return ""
-			}
-			k++
-			res += string('a' + i)
-			f[i]--
 		}
 	}
-	return res
+	return string(res)
 }
 
 //------------------------------------------------------------------------------
 // main
 
 func main() {
-	cases := [][]int{
-		{},
+	cases := []string{
+		"aab",
+		"aaab",
+		"vvvlo",
 	}
 
 	realCase := cases[0:]
 	for i, c := range realCase {
 		fmt.Println("## case", i)
 		// solve
-		fmt.Println(c)
+		fmt.Println(reorganizeString(c))
 	}
 }
